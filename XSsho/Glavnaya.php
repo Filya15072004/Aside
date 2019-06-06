544afed248a77eaf4af582b14118f2d551ef066e
Filya15072004 <51395761+Filya15072004@users.noreply.github.com>
2019-06-06 15:00:16 +0900

    Add files via upload

---
+++ XSsho/Glavnaya.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="bg-white">
+	<div class="container-fluid bg-info upline">
+<div class="row">
+		<a class="text-light" href="http://filya/XSsho/Glavnaya.php" class="">MAIN</a>
+
+		<a class="text-dark" href="" class="">JUMOR</a>
+
+		<a class="text-dark"  class="text-light"href="">PSIXO</a>
+
+		<a class="text-dark"  href="">IQ</a>
+
+		<a class="text-dark" href="">PHISICAL</a>
+	
+		<form class="form" method="POST" action="insert.php">
+			<input type="" name="">
+			<button>Добавить</button>
+		</form>
+		</div>
+	</div>
+
+<div class="container bg-light medline"></div>
+
+<script type="text/javascript">
+	
+</script>
+</body>
+</html>
